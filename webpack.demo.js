e226c3fea2e79b3e7d94dff287c17fe40c0b2006
Salvatore Laisa <moebiusmania@gmail.com>
2018-08-08 16:04:30 +0200

    fix import

--- webpack.demo.js
+++ webpack.demo.js
@@ -1,5 +1,6 @@
 
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
